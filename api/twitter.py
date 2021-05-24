a282dfd5fdac4ea0845f5b7413c3539bc9f2c52b
Gabriel Arruda <gda.gabriel@gmail.com>
2021-05-23 23:05:54 -0300

    building a wrapper to access Twitter API

---
+++ api/twitter.py
@@ -0,0 +1,50 @@
+import json
+import requests
+from requests.models import Response
+from config import settings
+import typing
+
+headers = {"Authorization": f"Bearer {settings.twitter.BEARER_TOKEN}"}
+
+def get(url: str, params: dict, headers: dict) -> Response:
+
+    response = requests.get(url, params=params, headers=headers)
+    response.raise_for_status()
+
+    return response
+
+def load_tweets(user_id: int, 
+                since_id: int = None) -> list:
+
+    url = f'https://api.twitter.com/2/users/{user_id}/tweets'
+
+    start_time = settings.crawler.BASE_DATE if not since_id else None
+ 
+    params = {'tweet.fields': 'in_reply_to_user_id,created_at',
+              'max_results': 100,
+              'start_time': start_time,
+              'since_id': since_id}
+
+    def load_tweets_token(tweets: list, 
+                          token: typing.Optional[str]) -> list:
+        
+        if token:
+            response = get(url,
+                           params={**params, 
+                                   **{'pagination_token': token}},
+                           headers=headers)
+
+            response.raise_for_status()
+            content = json.loads(response.text)
+
+            return load_tweets_token(tweets + content['data'],
+                                     content['meta'].get('next_token'))
+
+        else:
+            return tweets
+
+    response = get(url, params=params, headers=headers)
+    content = json.loads(response.text)
+
+    return load_tweets_token(content['data'],
+                             content['meta'].get('next_token'))
